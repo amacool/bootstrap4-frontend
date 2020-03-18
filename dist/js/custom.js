c08162bf61b2534d180941c9dd3e0253027956c2
Amacool <amacool0117@outlook.com>
2020-03-18 20:14:20 +0200

    built issue invoice modal

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -6,3 +6,8 @@ if ($('.data-table').length > 0) {
   $('.dataTables_wrapper>.row:last-child>div:last-child').removeClass('col-sm-12');
   $('.dataTables_wrapper>.row:last-child>div:last-child').removeClass('col-md-7');
 }
+
+$("#issue-invoice-btn").click(function () {
+  // $(".invite-send").removeClass("active");
+  $("body").addClass("custom-modal-open");
+});
