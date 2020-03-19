3ec6a8e0595822c5fd67739b0f8ff2705aa4c648
Amacool <amacool0117@outlook.com>
2020-03-19 19:28:38 +0200

    built request send-completion-report modal

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -8,6 +8,43 @@ if ($('.data-table').length > 0) {
 }
 
 $("#issue-invoice-btn").click(function () {
-  // $(".invite-send").removeClass("active");
   $("body").addClass("custom-modal-open");
+  $(".modal-dialog").removeClass("send-completion-modal-dialog");
+  $(".modal-dialog").addClass("issue-invoice-modal-dialog");
+});
+
+$("#send-completion-btn").click(function () {
+  $("body").addClass("custom-modal-open");
+  $(".modal-dialog").removeClass("issue-invoice-modal-dialog");
+  $(".modal-dialog").addClass("send-completion-modal-dialog");
+  $(".timesheet-wrapper").addClass("d-flex");
+  $(".timesheet-wrapper").removeClass("d-none");
+  $(".timesheet-tracker-wrapper").addClass("d-none");
+  $(".timesheet-tracker-wrapper").removeClass("d-flex");
+  $(".timesheet-video-wrapper").addClass("d-none");
+  $(".timesheet-video-wrapper").removeClass("d-flex");
+});
+
+$(".timesheet-tracker-card").click(function () {
+  $(".timesheet-wrapper").removeClass("d-flex");
+  $(".timesheet-wrapper").addClass("d-none");
+  $(".timesheet-tracker-wrapper").removeClass("d-none");
+  $(".timesheet-tracker-wrapper").addClass("d-flex");
+});
+
+$(".timesheet-video-card").click(function () {
+  $(".timesheet-wrapper").removeClass("d-flex");
+  $(".timesheet-wrapper").addClass("d-none");
+  $(".timesheet-video-wrapper").removeClass("d-none");
+  $(".timesheet-video-wrapper").addClass("d-flex");
+});
+
+$(".info-btn").click(function () {
+  $(".request-header-info").addClass("d-flex");
+  $(".request-header-info").removeClass("d-none");
+});
+
+$(".request-header-info").click(function () {
+  $(".request-header-info").removeClass("d-flex");
+  $(".request-header-info").addClass("d-none");
 });
