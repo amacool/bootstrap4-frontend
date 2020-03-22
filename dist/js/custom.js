63296823c42bd92ae1e1903742b313b4d9e157a3
Amacool <amacool0117@outlook.com>
2020-03-22 01:40:44 +0100

    built new request modal

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -10,12 +10,21 @@ if ($('.data-table').length > 0) {
 $("#issue-invoice-btn").click(function () {
   $("body").addClass("custom-modal-open");
   $(".modal-dialog").removeClass("send-completion-modal-dialog");
+  $(".modal-dialog").removeClass("new-request-modal-dialog");
   $(".modal-dialog").addClass("issue-invoice-modal-dialog");
+});
+
+$("#new-request-btn").click(function () {
+  $("body").addClass("custom-modal-open");
+  $(".modal-dialog").removeClass("send-completion-modal-dialog");
+  $(".modal-dialog").removeClass("issue-invoice-modal-dialog");
+  $(".modal-dialog").addClass("new-request-modal-dialog");
 });
 
 $("#send-completion-btn").click(function () {
   $("body").addClass("custom-modal-open");
   $(".modal-dialog").removeClass("issue-invoice-modal-dialog");
+  $(".modal-dialog").removeClass("new-request-modal-dialog");
   $(".modal-dialog").addClass("send-completion-modal-dialog");
   $(".timesheet-wrapper").addClass("d-flex");
   $(".timesheet-wrapper").removeClass("d-none");
@@ -72,3 +81,75 @@ $(".contact-send-completion").click(function () {
   }
   $(".request-send-completion").addClass("d-flex");
 });
+
+// custom select
+var x, i, j, selElmnt, a, b, c;
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  if (selElmnt) {
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < selElmnt.length; j++) {
+      c = document.createElement("DIV");
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener("click", function (e) {
+        var y, i, k, s, h;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < s.length; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            for (k = 0; k < y.length; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+  }
+}
+function closeAllSelect(elmnt) {
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
+
+// input for phone number 
+var input = document.querySelector("#phone");
+if (input) {
+  window.intlTelInput(input, {
+    dropdownContainer: document.body,
+    utilsScript: "./js/utils.js",
+  });
+}
